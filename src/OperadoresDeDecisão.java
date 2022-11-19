c93ded3ed7801948c610c0455ddafa305e894db9
Lupesms97 <lupesms97@gmail.com>
2022-11-18 22:24:56 -0300

    exercicios

--- src/OperadoresDeDecisão.java
+++ src/OperadoresDeDecisão.java
@@ -14,13 +14,33 @@ public class OperadoresDeDecisão {
         int variavel2 = scanner.nextInt();
 
         if(variavel1 == variavel2) {
-            System.out.println("A variavel 1 é igual");
+            System.out.println("O numero "+ variavel1 +" é igual ao número "+ variavel2);
         }else if(variavel1 > variavel2) {
-            System.out.println("A variavel 1 é maior");
+            System.out.println("O numero "+ variavel1 +" é mmaior que o número "+ variavel2);
         }else {
-            System.out.println("A variavel 1 é menor");
+            System.out.println("O numero "+ variavel1 +" é menor que o número "+ variavel2);
         }
 
+        System.out.println("Digite o primeiro nome: ");
+        String nome = scanner.next();
+
+        System.out.println("Digite o seu sobrenome: ");
+        String sobreNome = scanner.next();
+
+        if(nome == "Felipe" && sobreNome == "Mota") {
+            System.out.println("O seu nome é igual ao meu");
+        }else if(nome == "Felipe" || sobreNome == "Mota" ) {
+            System.out.println("Um dos seus nomes é igual ao meu");
+        }else{
+            System.out.println("Seus nomes são diferentes dos meus");
+        }
+
+        //Operadores ternarios
+        System.out.println("Digite sua altura:");
+        double altura = scanner.nextDouble();
+
+        String tamanho  = (altura > 1.70? "Você é alto" : "Você é baixo");
+        System.out.println(tamanho);
     }
 
 }
